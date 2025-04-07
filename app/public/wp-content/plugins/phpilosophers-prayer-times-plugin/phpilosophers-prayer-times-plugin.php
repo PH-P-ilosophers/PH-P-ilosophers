6ea38496bb1cd682dd6b6f4912a56e457a58f71f
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-04-06 23:24:13 -0400

    Activated the appropriate plugins

--- app/public/wp-content/plugins/phpilosophers-prayer-times-plugin/phpilosophers-prayer-times-plugin.php
+++ app/public/wp-content/plugins/phpilosophers-prayer-times-plugin/phpilosophers-prayer-times-plugin.php
@@ -24,7 +24,6 @@ class PT
     {
 
         add_shortcode('prayer-time', array($this, 'shortcode_callback'));
-        add_action('wp_head', array($this, 'prayer_time_page_setup'));
         add_action('wp_head', array($this, 'get_prayer_times'));
         add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
 
@@ -64,7 +63,6 @@ class PT
 
     public function shortcode_callback($atts)
     {
-
         ob_start();
 
         include plugin_dir_path(__FILE__) . 'templates/prayer-times.php';
@@ -86,34 +84,12 @@ class PT
         ));
     }
 
-    function generate_html()
-    {
-        if (is_page('prayer-time-page'))
-            include plugin_dir_path(__FILE__) . 'templates/prayer-times.php';
-    }
-
-    function generate_html2()
-    {
-        if (is_page('prayer-time-page'))
-            include plugin_dir_path(__FILE__) . 'templates/sampe.php';
-    }
-
 
     function register()
     {
-        add_action('admin_menu', array($this, 'add_admin_page'));
 
     }
 
-    function add_admin_page()
-    {
-        add_menu_page("Prayer Times Plugin", "Prayer Times", "manage_options", "prayer_times_plugin", array($this, "admin_index"), 'dashicons-clock', 110);
-    }
-
-    function admin_index()
-    {
-        require_once plugin_dir_path(__FILE__) . 'templates/prayer-times.php';
-    }
 
     function get_prayer_times()
     {
@@ -150,105 +126,16 @@ class PT
         }
     }
 
-    function display_prayer_times()
-    {
-
-        $result = $this->get_prayer_times();
-        $date_set = date('d-m-Y', strtotime('now'));
-
-        $this->generate_html();
-    }
-
-    function display_prayer_times2()
-    {
-
-        $result = $this->get_prayer_times();
-        $date_set = date('d-m-Y', strtotime('now'));
-
-        $this->generate_html2();
-    }
 
     public function enqueue_scripts()
     {
         wp_enqueue_style('prayer-times-style', plugin_dir_url(__FILE__) . 'assets/css/style.css', array(), '1.0.0');
-        
+
     }
 
 
 }
 
 $wp = new PT();
-$wp->register();
-
-
-// register_activation_hook(__FILE__, array($wp, 'activate') );
-
-
-
-
-
-// add_action( 'admin_menu', 'register' );
-// add_action( 'init', 'prayer_times_setup' );
-//     function prayer_times_menu() {
-//         add_options_page(
-//             'Prayer Times Settings',
-//             'Prayer Times',
-//             'manage_options',
-//             'prayer-times-settings',
-//             'prayer_times_settings_page',
-//         );
-//     }
-
-//     function prayer_times_plugin_register_settings() {
-//         register_setting('prayer_times_plugin_options', 'my_footer_message');
-//         add_settings_section(
-//             'prayer_times_plugin_section', 
-//             'Prayer Time Settings', 
-//             'prayer_times_plugin_section_callback', 
-//             'prayer-times-plugin',
-//         );
-
-//         add_settings_field(
-//             'my_footer_message', 
-//             'Footer Message', 
-//             'prayer_times_plugin_field_callback',  
-//             'prayer-times-plugin',
-//             'prayer_times_plugin_section',
-
-//         );
-//     }
-
-
-//     function phpilosophers_prayer_times_plugin () {
-//         phpinfo();
-
-//         if (is_page('events-page')) {
-//             echo "Hey";
-
-//             // From URL to get webpage contents.
-//             $url = "https://www.geeksforgeeks.org/";
-
-//             // Initialize a CURL session.
-//             $ch = curl_init(); 
-
-//             // Return Page contents.
-//             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-//             //grab URL and pass it to the variable.
-//             curl_setopt($ch, CURLOPT_URL, $url);
-
-//             $result = curl_exec($ch);
-
-//             echo $result; 
-//         }
-//     }
-// }
-
-// if (class_exists('PraryerTimesPlugin')) {
-//     $PrayTimesPlugin = new PrayerTimesPlugin();
-// }
-
-//     add_action( 'admin_menu', 'prayer_times_menu' );
-// }
 
 ?>
